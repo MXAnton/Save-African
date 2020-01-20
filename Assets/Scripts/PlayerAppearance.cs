d475abf9c932c148aa0688ca3fd87af610fde48a
MXAnton <anton.lehrberg@hotmail.se>
2020-01-20 20:24:55 +0100

    Made line rendered arms instead and made the player follow the bowl with a small margin

---
+++ Assets/Scripts/PlayerAppearance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAppearance : MonoBehaviour
+{
+    public LineRenderer leftArm;
+    public LineRenderer rightArm;
+
+    public Transform leftArmStartPosition;
+    public Transform rightArmStartPosition;
+
+    public Transform leftArmStopPosition;
+    public Transform rightArmStopPosition;
+
+    public Color armColor;
+
+    void Update()
+    {
+        UpdateArmLine(leftArm, leftArmStartPosition, leftArmStopPosition);
+
+        UpdateArmLine(rightArm, rightArmStartPosition, rightArmStopPosition);
+    }
+
+    void UpdateArmLine(LineRenderer line, Transform startPosition, Transform stopPosition)
+    {
+        line.startColor = armColor;
+        line.endColor = armColor;
+
+        line.SetPosition(0, startPosition.position);
+        line.SetPosition(1, stopPosition.position);
+    }
+}
