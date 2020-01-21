37ac8466aadf547dc3d665dda0dbf2ca676a674c
MXAnton <anton.lehrberg@hotmail.se>
2020-01-21 17:14:41 +0100

    Tuned the movement and made the head move nicely

---
+++ Assets/Scripts/Player/PlayerAppearance.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAppearance : MonoBehaviour
+{
+    float oldXPosition;
+    float newXPosition;
+
+    [Header("Head Vars")]
+    public Transform headTransform;
+    public float maxHeadRotation = 30;
+    public float headRotationMultiplier;
+
+    [Header("Arm Vars")]
+    public LineRenderer leftArm;
+    public LineRenderer rightArm;
+
+    public Transform leftArmStartPosition;
+    public Transform rightArmStartPosition;
+
+    public Transform leftArmStopPosition;
+    public Transform rightArmStopPosition;
+
+    public Color armColor;
+
+    private void Start()
+    {
+        newXPosition = transform.position.x;
+        oldXPosition = newXPosition;
+    }
+
+    void Update()
+    {
+        newXPosition = transform.position.x;
+        UpdateHeadTransform(oldXPosition, newXPosition, headRotationMultiplier, headTransform, maxHeadRotation);
+        oldXPosition = newXPosition;
+
+        UpdateArmLine(leftArm, leftArmStartPosition, leftArmStopPosition);
+
+        UpdateArmLine(rightArm, rightArmStartPosition, rightArmStopPosition);
+    }
+
+    static void UpdateHeadTransform(float oldPos, float newPos, float rotationMultiplier, Transform headTransform, float maxRotation)
+    {
+        float movementVelocity = oldPos - newPos;
+        float newZHeadRotation = movementVelocity * rotationMultiplier * 1000 * Time.deltaTime;
+
+        if (newZHeadRotation != 0)
+        {
+            if (newZHeadRotation > maxRotation)
+            {
+                newZHeadRotation = maxRotation;
+            }
+            else if (newZHeadRotation < -maxRotation)
+            {
+                newZHeadRotation = -maxRotation;
+            }
+            //headTransform.localEulerAngles = new Vector3(0, 0, -newZHeadRotation);
+            headTransform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(headTransform.localEulerAngles.z, -newZHeadRotation, 0.3f));
+        }
+        else
+        {
+            headTransform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(headTransform.localEulerAngles.z, 0, 0.03f));
+        }
+    }
+
+    void UpdateArmLine(LineRenderer line, Transform startPosition, Transform stopPosition)
+    {
+        line.startColor = armColor;
+        line.endColor = armColor;
+
+        line.SetPosition(0, startPosition.position);
+        line.SetPosition(1, stopPosition.position);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    public float movementMargin;
+
+    public Transform bowlTransform;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //float yDistance = -bowlTransform.position.y;
+        if (bowlTransform.position.x < transform.position.x - movementMargin)
+        {
+            rb2d.MovePosition(new Vector2(bowlTransform.position.x + movementMargin, transform.position.y));
+            //transform.position = new Vector2(bowlTransform.position.x + movementMargin, transform.position.y);
+        }
+        else if (bowlTransform.position.x > transform.position.x + movementMargin)
+        {
+            rb2d.MovePosition(new Vector2(bowlTransform.position.x - movementMargin, transform.position.y));
+            //transform.position = new Vector2(bowlTransform.position.x - movementMargin, transform.position.y);
+        }
+    }
+}
