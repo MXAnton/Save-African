ed85181efe2f5083928776027e2bd200c3f4b189
MXAnton <anton.lehrberg@hotmail.se>
2020-01-23 19:31:01 +0100

    Added main menu

---
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIController : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject settings;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowHighscores()
+    {
+        Debug.Log("Show highscores");
+    }
+
+    public void ShowSettings()
+    {
+        main.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void Back()
+    {
+        settings.SetActive(false);
+        main.SetActive(true);
+    }
+}
